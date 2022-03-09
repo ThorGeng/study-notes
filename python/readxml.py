64812e37aeed35143d42a04804cab67b2b833a71
耿金雷 <jinlei_geng@163.com>
2022-03-09 11:28:35 +0800

    完善readxml.py的注释
    Signed-off-by: 耿金雷 <jinlei_geng@163.com>

--- python/readxml.py
+++ python/readxml.py
@@ -1,5 +1,6 @@
 import shutil
 import zipfile
+import os
 from xml.dom.minidom import parse
 
 
@@ -8,13 +9,14 @@ def unzip_file(zip_path, unzip_path=None):
     :param zip_path: ofd格式文件路径
     :param unzip_path: 解压后的文件存放目录
     :return: unzip_path
+    解压至以文件名新建的文件夹
     """
     if not unzip_path:
         unzip_path = zip_path.split('.')[0]
     with zipfile.ZipFile(zip_path, 'r') as f:
         for file in f.namelist():
             f.extract(file, path=unzip_path)
-
+    
     return unzip_path
 
 def get_info(dir_path, unzip_file_path=None, removed=True):
@@ -23,21 +25,25 @@ def get_info(dir_path, unzip_file_path=None, removed=True):
     :param unzip_file_path: 解压后的文件路径
     :param removed: 是否删除解压后的目录
     :return: ofd_info，字典形式的发票信息
+    
     """
     file_path = unzip_file(dir_path, unzip_file_path)
     io = f"{file_path}/OFD.xml"
     element = parse(io).documentElement
-    nodes = element.getElementsByTagName('ofd:CustomDatas')
+    #通过tagname找到关键节点：element.getElementsByTagName()
+    nodes = element.getElementsByTagName('ofd:CustomDatas') 
     ofd_info = {}
+    #ofd:CustomDatas 只找到一个，len(nodes)=1    又有11个子节点，len(sun_node)=11
     for i in range(len(nodes)):
         sun_node = nodes[i].childNodes
         for j in range(len(sun_node)):
             name = sun_node[j].getAttribute('Name')
             value = sun_node[j].firstChild.data
             ofd_info[name] =value
+    #删除解压后的文件
     if removed:
         shutil.rmtree(file_path)
     return ofd_info
-    
-print(get_info(r"D:\Administrator\下载\电子发票\041002100113_04420071 - 副本.ofd"))
+
+print(get_info(os.getcwd()+r"\readxml.ofd"))
 
